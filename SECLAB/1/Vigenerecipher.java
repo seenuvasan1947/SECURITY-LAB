462592466d19b1d2d1d6c568359390086efac141
seenuvasan1947 <seenuthiruvpm@gmail.com>
2023-10-30 22:20:48 +0530

    8 exprement completed

--- SECLAB/1/Vigenerecipher.java
+++ SECLAB/1/Vigenerecipher.java
@@ -1,107 +1,42 @@
-
-import java.util.*;
-import java.io.*;
 public class Vigenerecipher
 {
-public static String key = new String();
-public String extndkey;
-public String plaintxt, ciphertxt;
-public String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// String plain,cipher;
-int row, flag=0, decrypt=0;
-public String keyextnsn(String ptxt,String keytxt)
+public static String encrypt(String text,final String key)
 {
-int i,j=0,n;
-String nukey="";
-for(i=0;i<ptxt.length();i++)
+String res="";
+text=text.toUpperCase();
+for(int i=0,j=0; i< text.length(); i++)
 {
-nukey += keytxt.charAt(j);
-j++;
-if (j == keytxt.length())
-j=0;
+char c=text.charAt(i);
+if(c<'A'||c>'z')
+continue;
+res+=(char)((c+key.charAt(j)-2*'A')%26+'A');
+j=++j%key.length();
 }
-return nukey;
+return res;
 }
-public int valueofchar(char x)
+public static String decrypt(String text,final String key)
 {
-int i,pos=0;
-for(i=0;i<26;i++)
+String res="";
+text=text.toUpperCase();
+for(int i=0,j=0;i<text.length();i++)
 {
-if ( x == ALPHABET.charAt(i))
-{
-
-pos = i;
-break;
+char c=text.charAt(i);
+if(c<'A'||c>'z')
+continue;
+res+=(char)((c-key.charAt(j)+26)%26+'A');
+j=++j%key.length();
 }
-}
-return pos;
-}
-public char charofvalue(int y)
-{
-int i;
-char ch;
-ch = ALPHABET.charAt(y);
-return ch;
-}
-public String vcencryption(String txt)
-{
-int i,j,p=0,k=0,tmp1=0;
-char tmp;
-String ctxt="";
-extndkey = keyextnsn(txt,key);
-System.out.println("VIGENERE ENCRYPTION");
-System.out.println("PLAIN TEXT : " + txt);
-System.out.println("VIGENERE KEY : " + extndkey);
-for(i=0;i<txt.length();i++)
-{
-p = valueofchar(txt.charAt(i));
-k = valueofchar(extndkey.charAt(i));
-// System.out.println("p : " + p + " k : " + k);
-tmp1 = ( p + k )%26;
-tmp = charofvalue(tmp1);
-// System.out.println("tmp1 : " + tmp1 + "tmp : " + tmp);
-ctxt += tmp;
-// System.out.println("CTXT : " + ctxt);
-}
-return ctxt;
-}
-public String vcdecryption(String txt)
-{
-int i,c=0,k=0,tmp1=0;
-char ch;
-
-String ptxt="";
-System.out.println("VIGENERE DECRYPTION");
-System.out.println("CIPHER TEXT : " + txt);
-System.out.println("VIGENERE KEY : " + extndkey);
-for(i=0;i<txt.length();i++)
-{
-c = valueofchar(txt.charAt(i));
-k = valueofchar(extndkey.charAt(i));
-tmp1 = ( c - k + 26 )%26;
-ch = charofvalue(tmp1);
-ptxt += ch;
-}
-return ptxt;
+return res;
 }
 public static void main(String[] args)
 {
-Vigenerecipher vc = new Vigenerecipher();
-Scanner sc = new Scanner(System.in);
-System.out.println("ENTER KEY");
-key = sc.next();
-String text = new String();
-System.out.println("Enter PLAIN TEXT");
-text = sc.next();
-String ciphertext = new String();
-ciphertext = vc.vcencryption(text);
-System.out.println();
-System.out.println("CIPHER TEXT :" + ciphertext);
-System.out.println();
-String plaintext = new String();
-plaintext = vc.vcdecryption(ciphertext);
-System.out.println();
-System.out.println("PLAIN TEXT :" + plaintext);
-sc.close();
+System.out.println("Enter the key: ");
+String key = System.console().readLine();
+System.out.println("Enter the message for encrytption: ");
+String message = System.console().readLine();
+String encryptedMsg=encrypt(message,key);
+System.out.println("String :"+message);
+System.out.println("Encrypted message:Cipher Text=" +encryptedMsg);
+System.out.println("Decrypted message:Plain Text="+decrypt(encryptedMsg,key));
 }
 }
